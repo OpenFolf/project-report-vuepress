d02bba4fdd3b1a50d3f9e52c2809f61c41d209a8
Aegir Aexx <aegir.tomasson@gmail.com>
2020-05-05 04:09:41 +0000

    Added PWA capabilities and Changed the TOC

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,20 @@
 module.exports = {
   title: 'openFOLF | Docs',
   description: 'Final Project BSc Computer Science @ Reykjavik University',
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#005737' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: '#005737' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#005737' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+  ],
   evergreen: true,
+  plugins: ['@vuepress/pwa', '@vuepress/back-to-top', '@vuepress/plugin-nprogress'],
   themeConfig: {
     displayAllHeaders: true,
     logo: '/images/openfolfgreen.svg',
@@ -12,7 +24,7 @@ module.exports = {
       { text: 'Source Code', link: 'https://github.com/OpenFolf/' },
       { text: 'Reykjavik University', link: 'https://www.ru.is/' },
     ],
-    searchPlaceholder: 'Search...',
+    searchPlaceholder: 'Search the report...',
     sidebar: [
       {
         collapsable: false,
@@ -32,6 +44,10 @@ module.exports = {
       },
       {
         collapsable: false,
+        children: ['/technical-overview/'],
+      },
+      {
+        collapsable: false,
         children: ['/design-overview/'],
       },
       {
@@ -40,7 +56,7 @@ module.exports = {
       },
       {
         collapsable: false,
-        children: ['/sprints/'],
+        children: ['/conclusion/'],
       },
     ],
   },
